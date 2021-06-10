77722630ca39495034491340ac160f88f8770923
ninjer94 <78516093+ninjer94@users.noreply.github.com>
2021-06-10 14:31:09 +1200

    RPS_retrieved

---
+++ 03_RPS_round_mechanics_v3.py
@@ -0,0 +1,13 @@
+# Main routine more efficient than v2
+
+
+
+def check_rounds():...
+    while True:
+        response = input("How many rounds: ")
+
+        round_error = "Please type either <enter> or an " \
+                      "integer that is more than 0\n"
+
+        # If infinity mode not chosen, check response
+        # is an integer that is
